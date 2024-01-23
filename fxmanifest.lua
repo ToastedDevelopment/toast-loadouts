28d84dd4378b33d33ce76c1ee0c9d4f5bfdfea7f
ToastedDevelopment <156986015+ToastedDevelopment@users.noreply.github.com>
2024-01-24 03:30:38 +1300

    Uploaded Main Files

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Vehicle Weapon Storage'
+author 'Toasted Development'
+version '1.0.0'
+description 'Store weapons in emergency service vehicles'
+
+client_scripts {
+    '@menuv/menuv.lua',
+    'stream/*.ytd',
+    'toast_loadout-menu.lua'
+}
